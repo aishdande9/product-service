165d0410bf2a40b30946c09df744b1c7a45bcd1e
Aishwarya <aish.dande@gmail.com>
2026-09-24 10:53:33 -0500

    classes and objects

--- Leetcode/src/problems/Student.java
+++ Leetcode/src/problems/Student.java
@@ -3,9 +3,9 @@ package problems;
 
 
 public class Student {
-    String name;
-    int age;
-    String course;
+    private String name;
+    private int age;
+    private String course;
 public Student(String name,int age,String course){
     this.name=name;
     this.age=age;
@@ -14,4 +14,23 @@ public Student(String name,int age,String course){
     public void displayDetails(){
         System.out.println("My name is "+ name + ". I am "+age+"years old."+ "I am studying "+course);
     }
+
+    public String getName(){
+    return this.name;
+    }
+
+
+    public int getAge(){
+    return this.age;
+    }
+
+    public String getCourse(){
+return this.course;
 }
+
+public void setCourse(String course){
+    this.course = course;
+}
+}
+
+
--- Leetcode/src/problems/StudentMain.java
+++ Leetcode/src/problems/StudentMain.java
@@ -4,5 +4,10 @@ public class StudentMain {
     public static void main(String[] args) {
         Student s = new Student("Aishwarya",37,"Java full stack");
         s.displayDetails();
+
+        System.out.println(s.getName());
+        System.out.println(s.getAge());
+        s.setCourse("python");
+        System.out.println(s.getCourse());
     }
 }
